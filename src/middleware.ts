d43af48a19b5f1b1c04498425d70a8bd8516b359
AbdulMoeezSultan <abdulmoeez0e@gmail.com>
2025-08-22 09:19:02 +0500

    changes 2

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ import { getToken } from "next-auth/jwt";
 export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
-    secret: process.env.AUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
   });
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/signin", request.url));
